d73d1fe32307de4c0e3ffad0c9e72c974744ead9
Sergey <sergey.melnikov@krit.pro>
2019-02-14 21:17:11 +0400

    Refactor db migtare

--- db/migrate/001_create_estimate_entries.rb
+++ db/migrate/001_create_estimate_entries.rb
@@ -1,20 +1,21 @@
 class CreateEstimateEntries < ActiveRecord::Migration
   def change
     create_table :estimate_entries do |t|
-      t.column :project_id,  :integer,  :null => false
-      t.column :user_id,     :integer,  :null => false
-      t.column :issue_id,    :integer
-      t.column :hours,       :float,    :null => false
-      t.column :comments,    :string,   :limit => 255
-      t.column :activity_id, :integer,  :null => false
-      t.column :spent_on,    :date,     :null => false
-      t.column :tyear,       :integer,  :null => false
-      t.column :tmonth,      :integer,  :null => false
-      t.column :tweek,       :integer,  :null => false
-      t.column :created_on,  :datetime, :null => false
-      t.column :updated_on, :datetime, :null => false
+      t.integer   "project_id",    null: false
+      t.integer   "user_id",       null: false
+      t.integer   "issue_id",     limit:  4
+      t.float     "hours",        limit: 24,   null: false
+      t.string    "comments",     limit: 255
+      t.integer   "activity_id",   null: false
+      t.date      "spent_on",      null: false
+      t.integer   "tyear",         null: false
+      t.integer   "tweek",         null: false
+      t.integer   "tmonth",        null: false
+      t.datetime  "created_on",    null: false
+      t.datetime  "updated_on",    null: false
     end
     add_index :estimate_entries, [:project_id], :name =>  :estimate_entries_project_id
     add_index :estimate_entries, [:issue_id],   :name =>  :estimate_entries_issue_id
   end
 end
+
